e586e5111b31b9011b8b9702d70aba671c2de531
Rodolfo Araujo <rodoufu@gmail.com>
2019-11-26 00:31:54 -0300

    Include, nullptr, using instead of typedef.

--- src/MyProjects/mainNPP-task.cpp
+++ src/MyProjects/mainNPP-task.cpp
@@ -31,29 +31,30 @@ using namespace optframe;
 using namespace NPP;
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("The expected use is:\n%s <input file> <seed>", argv[0]);
-        return EXIT_FAILURE;
-    }
-    unsigned seed;
-    string fileName(argv[1]);
-    sscanf("%u", argv[2], &seed);
-    if (!ifstream(fileName.c_str()).good()) {
-        printf("The file \"%s\" does not exist", fileName.c_str());
-        return EXIT_FAILURE;
-    }
+	if (argc != 3) {
+		printf("The expected use is:\n%s <input file> <seed>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	unsigned seed;
+	string fileName(argv[1]);
+	sscanf("%u", argv[2], &seed);
+	if (!ifstream(fileName.c_str()).good()) {
+		printf("The file \"%s\" does not exist", fileName.c_str());
+		return EXIT_FAILURE;
+	}
 
-    RandGenMersenneTwister rg(seed); // not using system rand() anymore
+	RandGenMersenneTwister rg(seed); // not using system rand() anymore
 
-    auto resp = solveSA(rg, fileName);
-    cout << "file;SA;timeSA;LS;timeLS" << endl;
-    cout <<
-         fileName << ";" <<
-         std::fixed << std::setprecision(0) << get<1>(resp).evaluation() << ";" <<
-         std::fixed << std::setprecision(5) << get<2>(resp) << ";" <<
-         std::fixed << std::setprecision(0) << get<4>(resp).evaluation() << ";" <<
-         std::fixed << std::setprecision(5) << get<5>(resp) << "" <<
-         endl;
+	auto resp = solveSA(rg, fileName);
+	cout << "file;size;SA;timeSA;LS;timeLS" << endl;
+	cout <<
+			 fileName << ";" <<
+			 get<0>(resp).getR().size() << ";" <<
+			 std::fixed << std::setprecision(0) << get<1>(resp).evaluation() << ";" <<
+			 std::fixed << std::setprecision(5) << get<2>(resp) << ";" <<
+			 std::fixed << std::setprecision(0) << get<4>(resp).evaluation() << ";" <<
+			 std::fixed << std::setprecision(5) << get<5>(resp) << "" <<
+			 endl;
 
-    return 0;
+	return 0;
 }
